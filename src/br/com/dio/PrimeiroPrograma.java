b4293ff2740703781971aa5eaa337f513a179308
rtcorradi <rubenscorradi@gmail.com>
2021-12-07 23:27:08 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Hello World!");*/
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+
+}
